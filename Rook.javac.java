746a6c0a757cd4be6d7cdf696a15bdf73630ba09
Sankalp ingle <sankalpingle007@gmail.com>
2025-09-28 17:28:41 +0530

    Delete Rook.javac.java

--- Rook.javac.java
+++
@@ -1,8 +0,0 @@
-@Override
-public List<Position> legalTargets(Board board, Position from) {
-    List<Position> res = new ArrayList<>();
-    Queen.addRays(board, from, res, new int[][]{
-        {-1,0},{1,0},{0,-1},{0,1}
-    });
-    return res;
-}
