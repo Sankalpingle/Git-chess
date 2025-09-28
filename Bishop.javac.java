a7d6a575c66d75d48afa7daa5041e31ccb148d3d
Sankalp ingle <sankalpingle007@gmail.com>
2025-09-28 17:23:21 +0530

    Delete Bishop.javac.java

--- Bishop.javac.java
+++
@@ -1,8 +0,0 @@
-@Override
-public ListPosition legalTargets(Board board, Position from) {
-    ListPosition res = new ArrayList();
-    Queen.addRays(board, from, res, new int[][]{
-        {-1,-1},{-1,1},{1,-1},{1,1}
-    });
-    return res;
-}
